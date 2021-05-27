314c9c253ae3450f40aa8acac0fffc8ca561675c
thong dev <thongkma001@gmail.com>
2021-05-27 17:56:46 +0700

    v-bind

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input v-bind:id="inputId" type="text" v-model="message">
+    {{ message }}
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      inputId: 'abc',
+      message: 'xin chao'
+    }
   }
 }
 </script>
